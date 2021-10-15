62555e1d8dd17a40c6fdf279848d54a0851332e3
xTheCoderx <justaguyoney@gmail.com>
2021-10-15 15:45:48 +1300

    Adding a file and writing a random number to it.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <iostream>
+#include <fstream>
+#include <direct.h>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::ofstream MyFile;
+
+    std::ofstream o("C:/Users/TO/Documents/Hi/pog.txt");
+
+    o << std::to_string(rand()) << std::endl;
+
     return 0;
 }
